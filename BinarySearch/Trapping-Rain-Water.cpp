0523465222d548e001994c06a6e12e57e91f92fc
PrithwiKumarSingh <prithwikumar871@gmail.com>
2026-04-04 16:01:04 +0530

    Trapping Rain Water

---
+++ BinarySearch/Trapping-Rain-Water.cpp
@@ -0,0 +1,51 @@
+
+// Trapping Rain Water -> https://leetcode.com/problems/trapping-rain-water/description/
+// Trapping Rain Water -> https://www.geeksforgeeks.org/trapping-rain-water/
+// ( Hard )
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Solving this problem in three passes Time Complexity: O(N)  Space Complexity: O(N)
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int n = height.size();
+
+        vector<int> leftMost(n,0);
+        vector<int> rightMost(n,0);
+
+        leftMost[0] = height[0], rightMost[n-1] = height[n-1];
+
+
+        // Prefix Max  -> fist pass
+        for(int i=1; i<n; i++){
+            leftMost[i] = max(leftMost[i-1],height[i]);
+        }
+
+        // Suffix Max  -> second pass
+        for(int i=n-2; i>0; i--){
+            rightMost[i] = max(rightMost[i+1],height[i]);
+        }
+
+        int totalWater = 0; 
+
+        // Third pass
+        for(int i=1; i<n-1; i++){
+            int minLeftRight = min(leftMost[i-1], rightMost[i+1]);
+
+            if(minLeftRight > height[i])
+            totalWater += minLeftRight-height[i];
+
+        }
+    
+    return totalWater;
+        
+    }
+};
+
+
+// Solving this problem in Two pass Time Complexity: O(N)  Space Complexity: O(n)
